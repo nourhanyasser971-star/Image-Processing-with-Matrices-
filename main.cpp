bf3b1e2235e771ce6e5216c3519a63685d2c09cc
Hagar Ahmed <ha3282987@gmail.com>
2026-09-20 02:36:15 +0300

    Add rotate90 function

--- main.cpp
+++ main.cpp
@@ -310,11 +310,15 @@ Image rotate90(const Image& input) {
     int height = input.getHeight();
     int width = input.getWidth();
     int channels = input.getChannels();
-    Image output(height, width, channels); // Width and height are swapped
+    Image output(height, width, channels); // العرض والطول متبدلين
 
-    // TODO: Implement this function
-    // For each pixel and each channel:
-    //   output(x, height-1-y, c) = input(y, x, c)
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            for (int c = 0; c < channels; c++) {
+                output(x, height - 1 - y, c) = input(y, x, c);
+            }
+        }
+    }
 
     return output;
 }
